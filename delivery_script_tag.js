fe2bd8c9438e38193677068f8e2390f649d252f2
tolu_vbox <tolu@handyjs.org>
2017-05-06 23:13:50 -0700

    modified delivery_script_tag.js

--- delivery_script_tag.js
+++ delivery_script_tag.js
@@ -1,12 +1,16 @@
 console.log('ver 1')
-window.onload = function(){
-  console.log('yay - script tag loaded')
-  jQuery(function() {
-    console.log(jQuery.datepicker)
-    jQuery("#date").datepicker( { 
-      minDate: +1, 
-      maxDate: '+2M',
-      beforeShowDay: jQuery.datepicker.noWeekends
+document.onreadystatechange = function(){
+  if(document.readyState === 'complate'){
+    console.log('yay - script tag loaded')
+    jQuery(function() {
+      console.log(jQuery.datepicker)
+      jQuery("#date").datepicker( { 
+        minDate: +1, 
+        maxDate: '+2M',
+        beforeShowDay: jQuery.datepicker.noWeekends
+      });
     });
-  });
+  } else {
+    console.log('waiting on document complete...' + document.readyState);
+  }
 }
