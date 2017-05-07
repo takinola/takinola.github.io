7835769d7c1cf203ed41577a14d4435773ddd05b
tolu_vbox <tolu@handyjs.org>
2017-05-06 23:56:25 -0700

    modified delivery_script_tag.js

--- delivery_script_tag.js
+++ delivery_script_tag.js
@@ -1,9 +1,10 @@
-console.log('ver 9')
-console.log(jQuery.fn.jQuery + ' version');
+console.log('ver 1')
+console.log(jQuery.fn.jquery + ' version - 1');
 
 const jQuery19 = jQuery.noConflict(true);
 (function(jQuery){
-  if(jQuery19.ui){console.log('jQuery ui loaded...')}
+  console.log(jQuery.fn.jquery + ' version - 2');
+  if(jQuery.ui){console.log('jQuery ui loaded...')}
     else {console.log('jQuery UI not found...')}
 
   console.log(jQuery.datepicker)
