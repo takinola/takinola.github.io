1b1f9d4c56f0b8a289015fb24ade98f518860842
Tolu Akinola <takinola@users.noreply.github.com>
2023-10-29 14:06:12 -0700

    Delete datepicker.js

--- datepicker.js
+++
@@ -1,8 +0,0 @@
-jQuery(function() {
-  console.log(jQuery.datepicker)
-  jQuery("#date").datepicker( { 
-    minDate: +1, 
-    maxDate: '+2M',
-    beforeShowDay: jQuery.datepicker.noWeekends
-  });
-});
